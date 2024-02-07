2987eb89bdc9e897e03b2d468f8b702a5084cc02
Mobina.Ghasempour <romi.ghasem@gmail.com>
2024-02-07 16:04:19 +0330

    created header fundumentals

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,21 @@
 
 const Header = () => {
-  console.log('it is header')
+
   return (
-    <></>
+    <>
+      <div className="header">
+        <div className="navbar">
+          <ul>
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Products</a></li>
+            <li><a>Contact</a></li>
+          </ul>
+        </div>
+        <div className="shopping-cart"></div>
+      </div>
+      <div className="header-img"></div>
+    </>
   )
 }
 export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 
 const Main = () => {
+
   return (
   <></>
 )
