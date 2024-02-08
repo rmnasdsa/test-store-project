7e3902be70f7f4ea040a407b09af09fb1a8bb17e
Mobina.Ghasempour <romi.ghasem@gmail.com>
2024-02-08 11:30:17 +0330

    trying to fetching data with promise

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,22 +1,28 @@
 
 const FetchData = () => {
 
-  const reject = () => console.log('error occured...');
-  const resolve = () => {
-    const data = fetch('../../data.json').then((res) => {
-      res.json();
-      // console.log(res);
-    }).then((jsonData) => console.log(jsonData))
-    // console.log(data);
+  // const reject = () => console.log('error occured...');
+  // const resolve = () => {
+  //   const data = fetch('../../data.json').then((res) => {
+  //     res.json();
+  //     // console.log(res);
+  //   }).then((jsonData) => console.log(jsonData))
+  //   // console.log(data);
  
-  }
-  const promise = new Promise((resolve) => {resolve()});
-  promise.then(() => {
-    setTimeout(resolve(), 1000);
-    // resolve()
-  }).catch (() => {
-    reject()
-  })
+  // }
+  // const promise = new Promise((resolve) => {resolve()});
+  // promise.then(() => {
+  //   setTimeout(resolve(), 1000);
+  //   // resolve()
+  // }).catch (() => {
+  //   reject()
+  // })
+
+
+
+  const promise = new Promise(() => {
+    const data = fetch('../../data.json')
+  }).then((res) => res.json()).then((jsonData) => {console.log(jsonData)}).catch((err) => console.log(err))
   return (
     <>
   
