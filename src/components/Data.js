f36de76eb60e9f4ca0342e725c5a83f918bd85ec
Mobina.Ghasempour <romi.ghasem@gmail.com>
2024-02-08 16:35:27 +0330

    some products added in main file

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,18 +1,21 @@
 export const products = [
   {
     id : 1,
-    name : "shoulder bag",
-    cost : "23 $"
+    name : "shoulder bags",
+    cost: "23 $",
+    img:'https://image-marketing.s3.ap-south-1.amazonaws.com/wp-content/uploads/2022/02/24170825/Types-Of-Bags-1-1068x580.jpg.webp'
   },
   {
     id :2,
-    name : "B",
-    cost : "12 $"
+    name : "work bag",
+    cost: "12 $",
+    img : 'https://www.briggs-riley.com/cdn/shop/articles/02_5.jpg?v=1608575147'
   },
   {
     id : 3,
-    name : "C",
-    cost : "70 $"
+    name : "baseball bag",
+    cost: "70 $",
+    img: 'https://media.boombah.com/image/upload/t_w695/banner-bat-bag-slider-sp-bat-pack-1'
   }
 ]
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,17 +5,12 @@ const Main = () => {
     <>
       <div className="container">
         <div className="products">
-          {/* <div className="product">
-            <img src='https://nypost.com/wp-content/uploads/sites/2/2023/10/Untitled-2000-x-1333-px-2023-11-29T122527.482.jpg' style={{width:'200px'}}></img>
-            <h1>{products[0].name}</h1>
-            <h3>{ products[0].cost}</h3>
-          </div> */}
-
-          {products.map(({product}) => {
+          {products.map(({ name, cost ,id, img}) => {     
             return (
-                <div className='product' key={Math.random()}>
-                  <img></img>
-                  <h1>{product[0]}</h1>
+                <div className='product' key={id}>
+                  <img src={img} style={{width: '200px'}}></img>
+                  <p>{ name}</p>
+                  <h4>{ cost}</h4>
                 </div>
             )
           })}
