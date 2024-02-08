28fb2da7c5037dd1d9ac186f2a11156aa5eccf6c
Mobina.Ghasempour <romi.ghasem@gmail.com>
2024-02-08 15:20:02 +0330

    trying to fix error about fetch data

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,6 +1,7 @@
 
 const FetchData = () => {
 
+  
   // const reject = () => console.log('error occured...');
   // const resolve = () => {
   //   const data = fetch('../../data.json').then((res) => {
@@ -20,12 +21,42 @@ const FetchData = () => {
 
 
 
-  const promise = new Promise(() => {
-    const data = fetch('../../data.json')
-  }).then((res) => res.json()).then((jsonData) => {console.log(jsonData)}).catch((err) => console.log(err))
+  // const promise = new Promise((resolve, reject) => {
+  //   const data = fetch(' https://ghibliapi.herokuapp.com/').then((res) => {return res.json()}).then((r) => console.log(r))
+  //   setTimeout(() => {
+  //     resolve(data)
+  //   },0)
+  // })
+  // promise.then((res) => {
+  //   return res;
+  // }).then((jsonData) => { console.log(jsonData); }).catch(
+  //   (err) => console.log(err))
+
+
+
+
+
+  const onFullFillment = async() => {
+    // const data = fetch('../../data.json', {
+    // method:"POST" })
+    //   const data = res.json();
+    //   data.then((jsonData) => console.log(jsonData))
+    // })
+
+    const data = await fetch('http://localhost:8000/data.json');
+    const res = await data.json();
+    console.log(res)
+  }
+  const onRejection = (err) => {console.log(err)}
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    },1000)
+  }).then(onFullFillment).catch(onRejection)
   return (
     <>
-  
+
     
     </>
   )
